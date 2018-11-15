1443ed04f5c116f17c5193e86a0314e90e42e043
Jean Oliveira-Courser <jeanxandre@gmail.com>
2018-11-15 16:26:15 -0300

    HTML Canvas

--- js/HTML5Tags.js
+++ js/HTML5Tags.js
@@ -1,60 +1,20 @@
 
-var audio = document.getElementById("audio");
-function setupSeek() {
-    var seek = document.getElementById("audioSeek");
-    seek.min = 0;
-    seek.max = Math.round(audio.duration);
-    seek.value = 0;
-    var duration = document.getElementById("duration");
-    duration.innerHTML = "0/" + Math.round(audio.duration);
-    }
-    function togglePlay() {
-        if (audio.paused || audio.ended) {
-        audio.play();
-        }
-        else {
-        audio.pause();
-        }
-        }
-        function updatePlayPause() {
-            var play = document.getElementById("play");
-            if (audio.paused || audio.ended) {
-            play.value = "Play";
-            }
-            else {
-            play.value = "Pause";
-            }
-            }
-            function endAudio() {
-                document.getElementById("play").value = "Play";
-                document.getElementById("audioSeek").value = 0;
-                document.getElementById("duration").innerHTML = "0/" + Math.round
-                (audio.duration);
-                }
-                // Wire-up the event handlers
-audio.addEventListener("durationchange", setupSeek, false);
-document.getElementById("play").addEventListener("click", togglePlay, false);
-audio.addEventListener("play", updatePlayPause, false);
-audio.addEventListener("pause", updatePlayPause, false);
-audio.addEventListener("ended", endAudio, false);
-function toggleMute() {
-    audio.muted = !audio.muted;
-    }
-    function updateMute() {
-        var mute = document.getElementById("mute");
-        if (audio.muted) {
-        mute.value = "Unmute";
-        }
-        else {
-        mute.value = "Mute";
-        }
-        }
-        function setVolume() {
-            var volume = document.getElementById("volume");
-            audio.volume = volume.value;
-            }
-            document.getElementById("mute").addEventListener("click",
-toggleMute, false);
-audio.addEventListener("volumechange", updateMute, false);
-document.getElementById("volume").addEventListener("change",
-setVolume, false);
+  window.onload = function()
+ {
+ // A1. CANVAS definition standard variables.
+ canvas = document.getElementById("canvasArea");
+ context = canvas.getContext("2d");
+ // A2. LAYOUT of first rectangle.
+ var xPos = 20; var yPos = 20;
+ var width = 100; var height = 50;
+ // A3. DISPLAY rectangles.
+ context.fillStyle = "hotpink";
+ context.fillRect (xPos, yPos, width, height);
+ context.lineWidth = 4;
+ context.strokeStyle = "royalblue";
+ context.strokeRect (xPos+130, yPos, width, height);
+ context.fillStyle = "darkorange";
+ context.fillRect (xPos+260, yPos, width, height);
+ context.clearRect (xPos+285, yPos+10, width-50, height-20);
+ }
+ 
