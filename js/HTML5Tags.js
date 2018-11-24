9ec0b0fb5d82b7ce7c6ff9102661b2f87bb85ede
Jean Oliveira-Courser <jeanxandre@gmail.com>
2018-11-24 20:48:51 -0300

    Animation css update

--- js/HTML5Tags.js
+++ js/HTML5Tags.js
@@ -1,20 +1,19 @@
-
-  window.onload = function()
- {
- // A1. CANVAS definition standard variables.
- canvas = document.getElementById("canvasArea");
- context = canvas.getContext("2d");
- // A2. LAYOUT of first rectangle.
- var xPos = 20; var yPos = 20;
- var width = 100; var height = 50;
- // A3. DISPLAY rectangles.
- context.fillStyle = "hotpink";
- context.fillRect (xPos, yPos, width, height);
- context.lineWidth = 4;
- context.strokeStyle = "royalblue";
- context.strokeRect (xPos+130, yPos, width, height);
- context.fillStyle = "darkorange";
- context.fillRect (xPos+260, yPos, width, height);
- context.clearRect (xPos+285, yPos+10, width-50, height-20);
- }
- 
+window.onload = function () {
+  // A1. CANVAS definition standard variables.
+  canvas = document.getElementById("canvasArea");
+  context = canvas.getContext("2d");
+  // A2. LAYOUT of first rectangle.
+  var xPos = 20;
+  var yPos = 20;
+  var width = 100;
+  var height = 50;
+  // A3. DISPLAY rectangles.
+  context.fillStyle = "hotpink";
+  context.fillRect(xPos, yPos, width, height);
+  context.lineWidth = 4;
+  context.strokeStyle = "royalblue";
+  context.strokeRect(xPos + 130, yPos, width, height);
+  context.fillStyle = "darkorange";
+  context.fillRect(xPos + 260, yPos, width, height);
+  context.clearRect(xPos + 285, yPos + 10, width - 50, height - 20);
+}
